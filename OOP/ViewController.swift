7296777155eb535cf8bfd12c6f057a70b1f15251
jamesivaniriyanto <James.iriyanto24@gmail.com>
2019-05-15 13:21:10 +0700

    Initial Commit

---
+++ OOP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OOP
+//
+//  Created by James Ivan Iriyanto on 15/05/19.
+//  Copyright © 2019 James Ivan Iriyanto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
